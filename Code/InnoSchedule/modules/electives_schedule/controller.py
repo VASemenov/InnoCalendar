7db022e91547edf338854994f769b406294b979b
Daniel <buh.add@gmail.com>
2019-11-14 23:08:05 +0300

    Make electives select queries

--- Code/InnoSchedule/modules/electives_schedule/controller.py
+++ Code/InnoSchedule/modules/electives_schedule/controller.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import datetime
+from sqlalchemy import func
 
 from modules.electives_schedule.classes import ElectivesUser, ElectivesLesson, ElectivesInfo
 from modules.core.source import db_read, db_write
@@ -27,3 +28,22 @@ def set_electives_user(session, user_id, subject):
 @db_read
 def get_user(session, user_id):
     return session.query(ElectivesUser).filter_by(id=user_id).first()
+
+
+@db_read
+def get_day_elective_lessons(session, user_id, day):
+    """
+    Function return elective lessons for user on exact weekday sorted by start time
+    :param session: sqlalchemy session from decorator
+    :param user_id:  int
+    :param day:  int [0-6]
+    :return: [Lesson]
+    """
+    monday = datetime.date.today() - datetime.timedelta(days=day-1)
+    this_week_dates = [monday]
+    for i in range(1, 7):
+        this_week_dates.append(monday + datetime.timedelta(i))
+    all_user_electives = list(i[0] for i in session.query(ElectivesUser).filter_by(user_id=user_id).values('subject'))
+    today_lessons = set(session.query(ElectivesInfo).filter(ElectivesInfo.subject.in_(all_user_electives), func.DATE(ElectivesInfo.day).in_(this_week_dates)).values('subject', 'day'))
+
+    return sorted([i[0] for i in today_lessons])

d977bf1bea34ba9b6e2706aaaeb2f864cdab16b2
Konstantin <britikovki@yandex.ru>
2019-11-25 21:24:03 +0300

    Visuals

--- Code/InnoSchedule/modules/electives_schedule/controller.py
+++ Code/InnoSchedule/modules/electives_schedule/controller.py
@@ -1,6 +1,5 @@
 import datetime
 from datetime import datetime as dt
-from sqlalchemy import func
 
 from modules.electives_schedule.classes import ElectivesUser, ElectivesLesson, ElectivesInfo
 from modules.core.source import db_read, db_write
@@ -13,15 +12,22 @@ def get_electives_course(session):
     return lessons
 
 
+
 @db_read
-def get_today_electives_courses(session, user_id):
+def get_day_elective_lessons(session, user_id, day):
+    today = dt.today()
+    today = today.replace(hour=0, second=0, microsecond=0, minute=0)
+    if day > today.weekday():
+        today = today + datetime.timedelta(days=day - today.weekday())
+    elif day < today.weekday():
+        today = today + datetime.timedelta(days=day - today.weekday() + 7)
+
     lessons = []
     courses = session.query(ElectivesUser.subject).filter_by(user_id=user_id).all()
-    day = dt.today().replace(hour=0, minute=0, second=0, microsecond=0)
     today_electives = session.query(ElectivesInfo.subject, ElectivesInfo.day, ElectivesInfo.room,
                                     ElectivesInfo.teacher,
                                     ElectivesInfo.start, ElectivesInfo.end).all()
-    today_electives = [elec for elec in today_electives if elec.day == day]
+    today_electives = [elec for elec in today_electives if elec.day == today]
     for i in today_electives:
         for j in courses:
             if i.subject == j.subject:
@@ -29,6 +35,7 @@ def get_today_electives_courses(session, user_id):
     lessons = [Lesson("Elective",lesson.subject, lesson.teacher, lesson.day, lesson.start, lesson.end, lesson.room) for
                lesson in lessons]
     return lessons
+
 
 
 @db_read
@@ -48,22 +55,3 @@ def set_electives_user(session, user_id, subject):
 @db_read
 def get_user(session, user_id):
     return session.query(ElectivesUser).filter_by(id=user_id).first()
-
-
-@db_read
-def get_day_elective_lessons(session, user_id, day):
-    """
-    Function return elective lessons for user on exact weekday sorted by start time
-    :param session: sqlalchemy session from decorator
-    :param user_id:  int
-    :param day:  int [0-6]
-    :return: [Lesson]
-    """
-    monday = datetime.date.today() - datetime.timedelta(days=datetime.date.today().isoweekday()-1)
-    this_week_dates = [monday]
-    for i in range(1, 7):
-        this_week_dates.append(monday + datetime.timedelta(i))
-    all_user_electives = list(i[0] for i in session.query(ElectivesUser).filter_by(user_id=user_id).values('subject'))
-    today_lessons = set(session.query(ElectivesInfo).filter(ElectivesInfo.subject.in_(all_user_electives), func.DATE(ElectivesInfo.day) == this_week_dates[day]).values('subject', 'day'))
-
-    return sorted([i[0] for i in today_lessons])
